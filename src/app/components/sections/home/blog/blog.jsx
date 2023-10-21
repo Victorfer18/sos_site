a5c20c2362b7b11855e16e6d23d5011322b21f06
victor fernando <victorfernandomagalhaes@gmail.com>
2023-10-20 21:59:35 -0300

    fix

---
+++ src/app/components/sections/home/blog/blog.jsx
@@ -0,0 +1,11 @@
+export default function Blog() {
+  return (
+    <section>
+      <div>
+        <div className="flex items-center justify-center gap-1 font-bold text-3xl">
+          <h2>Confira nossas novidades em nosso blog.</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import HeaderHome from "../components/sections/home/headerHome/headerHome.jsx";
 import OurServices from "../components/sections/home/ourServices/ourServices.jsx";
 import ReportMaintenance from "../components/sections/home/reportMaintenance/reportMaintenance.jsx";
-import Branchs from "../components/sections/home/branchs/brantchs.jsx";
+import Branchs from "../components/sections/home/branchs/branchs.jsx";
 import Certificates from "../components/sections/home/certificates/certificates.jsx";
 import OurCollaborator from "../components/sections/home/ourCollaborator/ourCollaborator.jsx";
+import Blog from "../components/sections/home/blog/blog.jsx";
 import Template from "../components/template.jsx";
 import { Spacer } from "@nextui-org/react";
 import TemplateApp from "../components/templateApp.jsx";
@@ -26,6 +27,7 @@ export default function Home() {
           <Spacer y={32} />
           <OurCollaborator />
           <Spacer y={32} />
+          <Blog />
           <Spacer y={32} />
           <Spacer y={32} />
         </Template>
