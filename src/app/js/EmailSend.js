8d88bcd87fc0566ddb0a7e980bd74b378c293574
unknown <victorfernandomagalhaes@gmail.com>
2024-01-29 16:58:28 -0300

    fix

--- src/app/js/EmailSend.js
+++ src/app/js/EmailSend.js
@@ -4,16 +4,20 @@ const nodemailer = require("nodemailer");
 
 export async function EmailSend(to, subject, message) {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    // host: process.env.EMAIL_HOST,
+    host: "smtp.titan.email",
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
+      // user: process.env.EMAIL_USER,
+      user: "servico@ddccompany.com.br",
+      // pass: process.env.EMAIL_PASSWORD,
+      pass: "servico@6969",
     },
   });
 
   const mailOptions = {
-    from: `SOS-SITE <${process.env.EMAIL_FROM}>`,
+    // from: `SOS-SITE <${process.env.EMAIL_FROM}>`,
+    from: `SOS-SITE <${"servico@ddccompany.com.br"}>`,
     to: to,
     subject: subject,
     html: message,
